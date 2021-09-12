e283f91974f64b518a6e96167b2f8b4c97045aef
OriPoria <58737667+OriPoria@users.noreply.github.com>
2021-09-12 10:39:25 +0300

    .

--- EM Analyzer/ModelClasses/ConfigurationService.cs
+++ EM Analyzer/ModelClasses/ConfigurationService.cs
@@ -17,6 +17,9 @@ namespace EM_Analyzer.ModelClasses
         public const string Minimum_Number_Of_Fixations_For_First_Pass = "Minimum_Number_Of_Fixations_For_First_Pass";
         public const string Minimum_Duration_Of_Fixation_For_First_Pass = "Minimum_Duration_Of_Fixation_For_First_Pass";
 
+        public const string Minimum_Number_Of_Fixations_For_Skip = "Minimum_Number_Of_Fixations_For_Skip";
+        public const string Minimum_Duration_Of_Fixation_For_Skip = "Minimum_Duration_Of_Fixation_For_Skip";
+
         public const string Minimum_Number_Of_Fixations_For_Page_Visit = "Minimum_Number_Of_Fixations_For_Page_Visit";
         public const string Minimum_Duration_Of_Fixation_For_Page_Visit = "Minimum_Duration_Of_Fixation_For_Page_Visit";
 
@@ -32,9 +35,7 @@ namespace EM_Analyzer.ModelClasses
         public const string Standard_Deviation = "Standard_Deviation";
         public const string Excel_Files_Extension = "Excel_Files_Extension";
         public const string Logs_Excel_File_Name = "Logs_Excel_File_Name";
-        
-        public const string Minimum_Number_Of_Fixations_For_Skip = "Minimum_Number_Of_Fixations_For_Skip";
-        public const string Minimum_Duration_Of_Fixation_For_Skip = "Minimum_Duration_Of_Fixation_For_Skip";
+
         
         public const string Second_File_Filtering_denominator = "Second_File_Filtering_denominator";
 
@@ -66,6 +67,12 @@ namespace EM_Analyzer.ModelClasses
         public static string MinimumDurationOfFixationForRegression { get { return GetValue(Minimum_Duration_Of_Fixation_For_Regression); } }
         public static string MinimumNumberOfFixationsForFirstPass { get { return GetValue(Minimum_Number_Of_Fixations_For_First_Pass); } }
         public static string MinimumDurationOfFixationForFirstPass { get { return GetValue(Minimum_Duration_Of_Fixation_For_First_Pass); } }
+        public static string MinimumNumberOfFixationsForSkip { get { return GetValue(Minimum_Number_Of_Fixations_For_Skip); } }
+        public static string MinimumDurationOfFixationForSkip { get { return GetValue(Minimum_Duration_Of_Fixation_For_Skip); } }
+        public static string MinimumNumberOfFixationsForPageVisit { get { return GetValue(Minimum_Number_Of_Fixations_For_Page_Visit); } }
+        public static string MinimumDurationOfFixationForPageVisit { get { return GetValue(Minimum_Duration_Of_Fixation_For_Page_Visit); } }
+
+
         public static string NumberOfFixationsOutAOIForException { get { return GetValue(Number_Of_Fixations_Out_AOI_For_Exception); } }
         public static string NumberOfFixationsInOfAOIForException { get { return GetValue(Number_Of_Fixations_In_Of_AOI_For_Exception); } }
         public static string DealingWithExceptionsLimitInPixels { get { return GetValue(Dealing_With_Exceptions_Limit_In_Pixels); } }
@@ -76,8 +83,6 @@ namespace EM_Analyzer.ModelClasses
         public static string StandardDeviation { get { return GetValue(Standard_Deviation); } }
         public static string ExcelFilesExtension { get { return GetValue(Excel_Files_Extension); } }
         public static string LogsExcelFileName { get { return GetValue(Logs_Excel_File_Name); } }
-        public static string MinimumNumberOfFixationsForSkip {  get { return GetValue(Minimum_Number_Of_Fixations_For_Skip); } }
-        public static string MinimumDurationOfFixationForSkip { get { return GetValue(Minimum_Duration_Of_Fixation_For_Skip); } }
         public static string SecondFileFilteringDenominator { get { return GetValue(Second_File_Filtering_denominator); } }
         public static string AnalyzeExtent { get { return GetValue(Analyze_Extent); } }
         public static string PreviewLimit { get { return GetValue(Preview_Limit); } }
