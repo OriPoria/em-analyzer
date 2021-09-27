dc40a853e270d8c972111fd0693db95b586ba803
OriPoria <58737667+OriPoria@users.noreply.github.com>
2021-09-27 14:20:25 +0300

    .

--- EM Analyzer/Program.cs
+++ EM Analyzer/Program.cs
@@ -148,8 +148,8 @@ namespace EM_Analyzer
             // Test mode- choose ahead the files
             else
             {
-                phrasesTextFilePath = @"C: \Users\oripo\Desktop\work\EyeTracker\small_20lines_c.txt";
-                wordsTextFilePath = @"C: \Users\oripo\Desktop\work\EyeTracker\small_20lines_w.txt";
+                phrasesTextFilePath = @"C: \Users\oripo\Desktop\work\EyeTracker\20lines_c.txt";
+                wordsTextFilePath = @"C: \Users\oripo\Desktop\work\EyeTracker\20lines_w.txt";
             }
             readingExcelFile.Join();
             FixationsService.phrasesExcelFileName = phrasesExcelFilePath.Substring(phrasesExcelFilePath.LastIndexOf(@"\") + 1);
